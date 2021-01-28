1b0de7d46e62e3c2947ce25c90169bb1c68582e2
Eldar <elogon@gmail.com>
2021-01-28 02:15:53 -0500

    Change server port to 8081

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 // const hostname = process.env.HOST;
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 require('./controller/api-routes')(app);
 
